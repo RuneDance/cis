3860db28f23faf49bdcf81824833cddef5ece871
yuyongtao <smallbadoncelaugh@gmail.com>
2019-07-01 19:14:30 +0800

    add files by yyt

--- src/main/java/com/oa/cis/Application.java
+++ src/main/java/com/oa/cis/Application.java
@@ -1,13 +1,16 @@
 package com.oa.cis;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+@MapperScan(value="com.oa.cis.dao")
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
 }
--- src/main/java/com/oa/cis/service/impl/UserServiceImpl.java
+++ src/main/java/com/oa/cis/service/impl/UserServiceImpl.java
@@ -8,8 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@Service
+@Service(value = "userService")
 public class UserServiceImpl implements UserService {
+
     @Autowired
     private UserMapper userMapper;
 
